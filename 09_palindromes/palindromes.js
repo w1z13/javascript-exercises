e90c87a204da3d942b5a5168b52d38471cb1bd55
w1z13 <115799235+w1z13@users.noreply.github.com>
2022-10-31 15:36:39 +0100

    test passed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    strArray = input.toLowerCase().replaceAll(/\W/g,'').split('');
+    revArray = [...strArray].reverse();
+    if (strArray.toString() === revArray.toString()) return true;
+    return false;
 };
 
 // Do not edit below this line
