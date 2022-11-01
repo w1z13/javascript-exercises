4ecc73005e0ec5d235da85790877d25cd60a1893
w1z13 <115799235+w1z13@users.noreply.github.com>
2022-11-01 08:54:35 +0100

    test passed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,31 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    let current = 0;
+    let next = 1;
+    if (a >= 0){
+        for (i = 0; i < Number(a); i++){
+            let temp = current + next;
+            current = next;
+            next = temp;
+        }
+    return current;
+    }
+    return 'OOPS';
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+
+/*
+0 1     0th
+1 1     1st
+1 2     2nd
+2 3     3rd
+3 5     4th
+5 8     5th
+8 13    6th
+*/
+
+
+
+
