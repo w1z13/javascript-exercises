27c35994e378e5a338ec52065f42654bd37d9681
w1z13 <115799235+w1z13@users.noreply.github.com>
2022-11-01 09:44:51 +0100

    test passed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(input) {
+    temp = input.map((obj) => {
+        if(obj.yearOfDeath != undefined){
+            return obj;
+        } else {
+            obj.yearOfDeath = new Date().getFullYear();
+            return obj;
+        }
+    }).sort((obj1, obj2) => {
+        return obj1.yearOfDeath - obj1.yearOfBirth < obj2.yearOfDeath - obj2.yearOfBirth ? 1 : -1;
+    });
+    return temp[0];
 };
 
 // Do not edit below this line
